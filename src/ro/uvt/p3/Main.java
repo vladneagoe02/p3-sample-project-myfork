90e865cfed51345704ce94a6be8fe3894c80e5fb
vladneagoe02 <vlad.neagoe02@e-uvt.ro>
2022-10-19 19:00:47 +0300

    something

--- src/ro/uvt/p3/Main.java
+++ src/ro/uvt/p3/Main.java
@@ -4,7 +4,7 @@ import ro.uvt.p3.io.InputDevice;
 import ro.uvt.p3.io.OutputDevice;
 
 public class Main {
-
+// comment
     public static void main(String[] args) {
         InputDevice id = new InputDevice();
         OutputDevice od = new OutputDevice();
